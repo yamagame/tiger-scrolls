b3142af3639e6e88782a458f289bd9f1153a437d
Hiromitsu Yamaguchi <yamagame@mac.com>
2018-11-06 13:55:31 +0900

    更新

---
+++ precache-manifest.9a63a8d9de7e22fd2bc2b5929ccf3741.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e48c7f2440f3f837eaab",
+    "url": "/tiger-scrolls/static/js/runtime~main.e48c7f24.js"
+  },
+  {
+    "revision": "06f3c9ffe52d39d3fb17",
+    "url": "/tiger-scrolls/static/js/main.06f3c9ff.chunk.js"
+  },
+  {
+    "revision": "424105e79c599d13b737",
+    "url": "/tiger-scrolls/static/js/5.424105e7.chunk.js"
+  },
+  {
+    "revision": "70bc6c91ad517e501bd3",
+    "url": "/tiger-scrolls/static/js/4.70bc6c91.chunk.js"
+  },
+  {
+    "revision": "0a74b93f957862413ac9",
+    "url": "/tiger-scrolls/static/js/3.0a74b93f.chunk.js"
+  },
+  {
+    "revision": "7ee7a62e60ee449f7931",
+    "url": "/tiger-scrolls/static/js/2.7ee7a62e.chunk.js"
+  },
+  {
+    "revision": "59e22c40610b6dbf0cb0",
+    "url": "/tiger-scrolls/static/js/1.59e22c40.chunk.js"
+  },
+  {
+    "revision": "06f3c9ffe52d39d3fb17",
+    "url": "/tiger-scrolls/static/css/main.e9f96536.chunk.css"
+  },
+  {
+    "revision": "e947169a654a545917f21cb628357a73",
+    "url": "/tiger-scrolls/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/tiger-scrolls/precache-manifest.ec92c7167c84d06b6a1a3ae39e9eb759.js"
+  "/tiger-scrolls/precache-manifest.9a63a8d9de7e22fd2bc2b5929ccf3741.js"
 );
 
 workbox.clientsClaim();
